95bdb24f197236eb615d42ac0cb614027d94b8f5
Sebastian Valdivia Peceros <sebasvp2005@gmail.com>
2024-06-22 23:56:15 -0500

    fix provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router';
-import {NgIf} from "@angular/common";
-import {HeaderContentComponent} from "./public/components/header-content/header-content.component";
-import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import { NgIf } from "@angular/common";
+import { HeaderContentComponent } from "./public/components/header-content/header-content.component";
+import { FooterContentComponent } from "./public/components/footer-content/footer-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderContentComponent, FooterContentComponent, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'FindU';
@@ -20,14 +20,13 @@ export class AppComponent {
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.showNavFooter = !(event.url === '/login'|| event.url === '/register');
+        this.showNavFooter = !(event.url === '/login' || event.url === '/register');
 
         /*
         if (event.url === '/login') {
           localStorage.removeItem('id-temporal');
         }
         */
-
       }
     });
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from "./content/pages/login/login.component";
 import {RegisterComponent} from "./content/pages/register/register.component";
 import {HomeComponent} from "./content/pages/home/home.component";
 import {UsersChatComponent} from "./content/components/home-components/users-chat/users-chat.component";
+import { provideRouter } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -11,4 +12,9 @@ export const routes: Routes = [
   { path: 'home/users-chat', component: UsersChatComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+
+export const appRoutingProviders = [
+  provideRouter(routes)
 ];
